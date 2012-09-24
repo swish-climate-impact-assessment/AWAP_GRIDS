4c85928b53b67e13f817ec0b79c25dbc76cc97db
Ivan Hanigan <ivan.hanigan@gmail.com>
2012-09-24 22:02:19 +1000

    add stuff

--- run/connect2postgres.r
+++ run/connect2postgres.r
@@ -1,22 +1,36 @@
-connect2postgres <- function(hostip,db,user, p=NA, pgutils = 'i:/dropbox/tools/pgutils'){
-if(is.na(p)){
-pwd=readline("enter password (! it will be shown, use ctrl-L to clear the console after): ")
-} else {
-pwd <- p
-}
-if (!require(RJDBC)) install.packages('RJDBC'); require(RJDBC)
-
-# This downloads the JDBC driver to your selected directory if needed
-# older postgresql-8.4-701.jdbc4.jar
-
-if (!file.exists(file.path(pgutils,'postgresql-9.1-901.jdbc4.jar'))) {
-dir.create(pgutils,recursive =T)
-download.file("http://jdbc.postgresql.org/download/postgresql-9.1-901.jdbc4.jar",file.path(pgutils,'postgresql-9.1-901.jdbc4.jar'),mode="wb")
-}
-
-# connect
-pgsql <- JDBC( "org.postgresql.Driver", file.path(pgutils,"postgresql-9.1-901.jdbc4.jar"))
-con <- dbConnect(pgsql, paste("jdbc:postgresql://",hostip,"/",db,sep=""), user = user, password = pwd)
-rm(pwd)
-return(con)
-}
+# a function to connect to PostgreSQL 8.4
+connect2postgres <- function(hostip=NA,db=NA,user=NA, p=NA, os = 'linux', pgutils = c('/home/ivan/tools/jdbc','c:/pgutils')){
+ if(is.na(hostip)){
+ hostip=readline('enter hostip: ')
+ } 
+ if(is.na(db)){
+ db=readline('enter db: ')
+ }
+ if(is.na(user)){
+ user=readline('enter user: ')
+ }
+ if(is.na(p)){
+ pwd=readline(paste('enter password for user ',user, ': ',sep=''))
+ } else {
+ pwd <- p
+ }
+ if(os == 'linux'){
+  if (!require(RPostgreSQL)) install.packages('RPostgreSQL', repos='http://cran.csiro.au'); require(RPostgreSQL)
+  con <- dbConnect(PostgreSQL(),host=hostip, user= user, password=pwd, dbname=db)
+ } else { 
+  if (!require(RJDBC)) install.packages('RJDBC'); require(RJDBC) 
+  # This downloads the JDBC driver to your selected directory if needed
+  if (!file.exists(file.path(pgutils,'postgresql-8.4-701.jdbc4.jar'))) {
+  dir.create(pgutils,recursive =T)
+  download.file('http://jdbc.postgresql.org/download/postgresql-8.4-701.jdbc4.jar',file.path(pgutils,'postgresql-8.4-701.jdbc4.jar'),mode='wb')
+  }
+  # connect
+  pgsql <- JDBC( 'org.postgresql.Driver', file.path(pgutils,'postgresql-8.4-701.jdbc4.jar'))
+  con <- dbConnect(pgsql, paste('jdbc:postgresql://',hostip,'/',db,sep=''), user = user, password = pwd)
+ }
+ # clean up
+ rm(pwd)
+ return(con)
+ }
+ #  ch <- connect2postgres()
+ # enter password at console
