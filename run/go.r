0068bc337c5d3e655d4b15051e6ee8698e0f39b6
Ivan Hanigan <ivan.hanigan@gmail.com>
2012-12-16 08:18:46 +1100

    change to ProjectTemplate style

--- run/go.r
+++
@@ -1,4 +0,0 @@
-
-source(dir('run',pattern = 'tools', full.names=T))
-# source(dir('run',pattern = 'load', full.names=T))
-# source(dir('run',pattern = 'check', full.names=T))
--- run/overview.r
+++
@@ -1,9 +0,0 @@
-
-rootdir <- getwd()
-wd <- '~/data/AWAP_GRIDS'
-# source(dir('run',pattern = 'tools', full.names=T))
-# file.remove(dir('run',full.names=T))
-setwd(wd)
-dir()
-
-print('helloworld2')
