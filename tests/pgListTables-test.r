2b7c2412ae3d6f2d8cdae4781581f6958a9ad77c
Ivan Hanigan <ivan.hanigan@gmail.com>
2013-01-24 16:46:45 +1100

    testing

--- tests/pgListTables-test.r
+++ tests/pgListTables-test.r
@@ -3,7 +3,12 @@
 # name:pgListTables-test
 require(ProjectTemplate)
 load.project()
+
 require(swishdbtools)
 ch <- connect2postgres(h = '115.146.84.135', db = 'ewedb', user= 'ivan_hanigan')
 tbls <- pgListTables(conn=ch, schema='awap_grids', pattern='maxave')
-tbls$date <-
+tbls$date <- paste(substr(gsub("maxave_","",tbls[,1]),1,4),
+        substr(gsub("maxave_","",tbls[,1]),5,6),
+        substr(gsub("maxave_","",tbls[,1]),7,8),
+        sep="-")
+head(tbls)
