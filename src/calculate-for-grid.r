cd1c724af9232e226a9ebc81702e4d9dd56c7786
Ivan <ivan.hanigan@gmail.com>
2013-05-14 16:45:29 +1000

    add landmask

--- src/calculate-for-grid.r
+++ src/calculate-for-grid.r
@@ -80,7 +80,7 @@ enddate <- EndDate
 ch<-connect2postgres2("ewedb")
 tempTableName <- swish_temptable("ewedb")
 # address doesn't exist
-dbSendQuery(ch, sprintf("alter table gislibrary.%s add column address serial", tempTableName_locations))
+dbSendQuery(ch, sprintf("alter table %s.%s add column address serial",tempTableName$schema, tempTableName_locations))
 raster_extract_by_day(ch, startdate, enddate,
                       schemaName = tempTableName$schema,
                       tableName = tempTableName$table,
@@ -106,6 +106,42 @@ write.csv(output_data,outputDataFile, row.names = FALSE)
 outputFileName <- outputDataFile
 outputFileName
 
+
+################################################################
+# name: intersect pixels with coast
+pgListTables(ch, "boundaries_electorates")
+sql_subset(ch, 
+           "boundaries_electorates.electorates2009"
+           , limit = 1, eval = T)
+loc_tab <- paste(sep = "",tempTableName$schema,".",tempTableName_locations)
+sql <- sql_subset(ch, 
+           loc_tab
+           , limit = 1, eval = F)
+cat(sql)
+dbSendQuery(ch,
+           paste("
+select long, lat, t1.gid, address, elect_div 
+into tempfoobar
+from ", loc_tab, " t1,
+boundaries_electorates.electorates2009 t2
+where st_contains(t2.the_geom, t1.the_geom)
+                 ", sep = "")
+           )
+sql_subset(ch, paste(sep = ".", tempTableName$schema, "tempfoobar"), limit = 1, eval = T)
+dbSendQuery(ch,
+            paste("
+select t1.*, case when t2.elect_div is not null then 1 else 0 end as landmask 
+into tempfoobar1
+from ", loc_tab, " t1
+left join
+john_snow.tempfoobar t2
+on t1.address = t2.address
+", sep = "")
+            )
+df <- sql_subset(ch, paste(sep = ".", tempTableName$schema, "tempfoobar1"), limit = -1, eval = T)
+with(df, plot(long, lat, col = landmask, pch = 16))
+head(df)
+write.csv(df, "landmask.csv", row.names = F)
 ################################################################
 # name: visualise a pixel
 head(output_data)
