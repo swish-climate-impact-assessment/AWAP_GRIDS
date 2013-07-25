78d488427be5528c55343717a4e8b34e48a963dd
ivan <ivan.hanigan@gmail.com>
2013-07-25 20:49:00 +1000

    more testing

--- test-monthly.R
+++ test-monthly.R
@@ -17,27 +17,58 @@ cfiles <- list.files(pattern = '.tif', full.names=T)
 b <- brick(stack(cfiles))
 # broke
 b <- stack(cfiles)
-meansmosas <-  overlay(b,fun=function(x) movingFun(x, fun=sum,n=6, "to", na.rm=TRUE))
-str(meansmosas)
+#meansmosas <-  overlay(b,fun=function(x) movingFun(x, fun=sum,n=6, "to", na.rm=TRUE))
+# Error in file(fnamevals, "wb") : cannot open the connection
+# In addition: Warning message:
+#   In file(fnamevals, "wb") :
+#   cannot open file '/tmp/R_raster_tmp/ivan_hanigan/raster_tmp_2013-07-25_160646_04951.gri': No such file or directory
+#str(meansmosas)
 require(swishdbtools)
+if(!exists("pwd")) pwd <- getPassword(remote=T)
 shp <- readOGR2(h='brawn.anu.edu.au', d='ewedb',u='john_snow',
-                layer = 'weather_bom.combstats')
+                layer = 'weather_bom.combstats', pwd)
 plot(shp)
 # select a station
 shp@data[1,]
 ##   stnum                     name lat lon ele   new
 ## 1 91004 BRANXHOLM (SCOTT STREET) -41 147 185 FALSE
 shp2  <- shp[shp@data$stnum == 91004, ]
+sampled  <- sample(shp$stnum, 0.01 * nrow(shp))
+length(sampled)
+# 81
+shp2  <- shp[which(shp$stnum %in% sampled),]
 str(shp2)
-plot(shp2)
-e <- extract(meansmosas, shp2, df=T)
-str(e) ## print for debugging
+plot(shp2, add = T, col = 'red')
+axis(1); axis(2)
+# e <- extract(meansmosas, shp2, df=T)
+system.time(
+e <- extract(b, shp2, df=T)
+)
+# user  system elapsed 
+# 366.178   4.961 464.571 
+nrow(e[,1:3])
 
-#
-r1 <- readGDAL(cfiles[1])
-r2 <- readGDAL(cfiles[2])
-r3 <- readGDAL(cfiles[3])
-rasters1 <- list(r1, r2, r3)
-str(rasters1[[1]])
-rasters1[[1]][1][1]
-
+system.time(
+  b <- stack(cfiles)
+)
+system.time(
+  e <- extract(b, shp, df=T)
+)
+nrow(e[,1:3])
+output <- cbind(shp@data$stnum,e)
+df <- output[1:20,]
+df[,1:4]
+require(reshape2)
+require(awaptools)
+reformat_awap_data
+df2 <- melt(df, id.vars=c("shp@data$stnum","ID"))
+str(df2)
+head(df2)
+table(df2[,"shp@data$stnum"])
+# r1 <- readGDAL(cfiles[1])
+# r2 <- readGDAL(cfiles[2])
+# r3 <- readGDAL(cfiles[3])
+# rasters1 <- list(r1, r2, r3)
+# str(rasters1[[1]])
+# rasters1[[1]][1][1]
+# 
