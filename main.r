f339db407ae883f674cab2b48664739d09cbb605
ivanhanigan <ivan.hanigan@gmail.com>
2014-01-13 16:25:11 +1100

    mods re tern5 and brawn

--- main.r
+++ main.r
@@ -24,24 +24,14 @@ load.project()
 
 ####################
 # user definitions, or setup interactively
-destination_server <- "brawn.anu.edu.au"
-<<<<<<< HEAD
-  #"tern5.qern.qcif.edu.au" 
-source_server <- "tern5.qern.qcif.edu.au" 
-  #"115.146.92.162"
-fresh <- TRUE
-startdate <- '1990-01-01'
-enddate <-  Sys.Date()-2
-=======
-source_server <- "tern5.qern.qcif.edu.au" 
-#source_server <- "115.146.92.162"
+destination_server <- "tern5.qern.qcif.edu.au" 
+source_server <- "115.146.92.162"
 fresh <- FALSE
-startdate <- '1975-01-01'
+startdate <- '1980-01-01'
 enddate <-  '1980-02-01' #Sys.Date()-2
->>>>>>> 8d4a3f05c8b6a267145268b4a59518f408394a57
 checkDates <- TRUE
 interactively <- FALSE
-variablenames <- 'solarave' #totals' #vprph09,vprph15' #,solarave maxave,minave' #,totals,
+variablenames <- 'maxave,minave,totals,vprph09,vprph15' #,solarave
 aggregation_factor <- 3
 if(length(grep('linux',sessionInfo()[[1]]$os)) == 1)
 {
@@ -50,11 +40,7 @@ if(length(grep('linux',sessionInfo()[[1]]$os)) == 1)
   os <- 'windows'
 }
 #os <- 'linux' # only linux and windoze supported
-<<<<<<< HEAD
-pgisutils <- "/usr/pgsql-9.2/bin/"
-=======
-pgisutils <- "" #/usr/pgsql-9.1/bin/"
->>>>>>> 8d4a3f05c8b6a267145268b4a59518f408394a57
+pgisutils <- "/usr/pgsql-9.1/bin/"
 #"\"C:\\pgutils\\postgis-pg92-binaries-2.0.2w64\\bin\\"
 pgutils <- "\"C:\\pgutils\\pgsql\\bin\\"
 
@@ -65,10 +51,6 @@ if(fresh == TRUE)
 {
   source(file.path(workdir, "src/load.r"))  
 } else {
-  if(!exists('pwd'))
-  {
-    pwd <- getPassword(remote=T)
-  }
   source(file.path(workdir, "src/load_mirrored_grids.r"))  
 }
 
