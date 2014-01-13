04ffcee58d542df45cea7928759e8a7faa1dc83e
ivanhanigan <ivan.hanigan@gmail.com>
2014-01-13 16:38:43 +1100

    test download monthly

--- AWAP_GRIDS-monthly.r
+++ AWAP_GRIDS-monthly.r
@@ -1,14 +1,13 @@
 require(awaptools)
 require(swishdbtools)
 # local customisations
-workdir <- "/ResearchData/AWAP_GRIDS_RAIN_MONTHLY"
+workdir <- "data"
 setwd(workdir)
-#outdir <- "data"
-#setwd(outdir)
-start_date <- "1900-01-01"
-start_date <- as.POSIXlt(start_date)
+
+startdate <- "2013-11-01"
+enddate <- "2014-01-01"
 # do
-load_monthly(startdate = start_date)
+load_monthly(start_date = startdate, end_date = enddate)
 
 # do
 filelist <- dir(pattern = "grid.Z$")
@@ -16,4 +15,4 @@ for(fname in filelist)
 {
   unzip_monthly(fname, aggregation_factor = 1)
 }
-setwd(workdir)
+setwd("..")
