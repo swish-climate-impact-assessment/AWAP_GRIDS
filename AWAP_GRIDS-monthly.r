24976d0dd5dcc53f729e4aed9b630295d010d4d6
Ivan <ivan.hanigan@gmail.com>
2014-05-29 12:51:56 +1000

    add notes to get monthly

--- AWAP_GRIDS-monthly.r
+++ AWAP_GRIDS-monthly.r
@@ -1,11 +1,14 @@
+require(devtools)
+install_github("awaptools", "swish-climate-impact-assessment")
 require(awaptools)
+install_github("swishdbtools", "swish-climate-impact-assessment")
 require(swishdbtools)
 # local customisations
 workdir <- "data"
 setwd(workdir)
 
-startdate <- "2013-11-01"
-enddate <- "2013-12-31"
+startdate <- "2000-01-01"
+enddate <- "2012-12-31"
 # do
 load_monthly(start_date = startdate, end_date = enddate)
 
