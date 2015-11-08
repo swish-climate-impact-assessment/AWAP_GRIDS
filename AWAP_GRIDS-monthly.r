bcab55a6768dc769ee2cadb6654a0cafa163f5e5
ivanhanigan <ivan.hanigan@gmail.com>
2015-11-08 23:54:30 +1100

    add work on latest monthly dl

--- AWAP_GRIDS-monthly.r
+++ AWAP_GRIDS-monthly.r
@@ -1,14 +1,14 @@
 require(devtools)
-install_github("awaptools", "swish-climate-impact-assessment")
+install_github("swish-climate-impact-assessment/awaptools")
 require(awaptools)
-install_github("swishdbtools", "swish-climate-impact-assessment")
+#install_github("swishdbtools", "swish-climate-impact-assessment")
 require(swishdbtools)
 # local customisations
 workdir <- "data"
 setwd(workdir)
-
-startdate <- "2000-01-01"
-enddate <- "2012-12-31"
+dir()
+startdate <- "2014-01-01"
+enddate <- "2015-10-30"
 # do
 load_monthly(start_date = startdate, end_date = enddate)
 
@@ -16,6 +16,13 @@ load_monthly(start_date = startdate, end_date = enddate)
 filelist <- dir(pattern = "grid.Z$")
 for(fname in filelist)
 {
+  #fname = filelist[1]
+  #fname
   unzip_monthly(fname, aggregation_factor = 1)
+  fname <- dir(pattern = "grid$")
+  r <- readGDAL(fname)
+  outfile <- gsub('.grid', '.tif', fname)
+  writeGDAL(r, outfile, drivername="GTiff")
+  file.remove(fname)
 }
 setwd("..")
